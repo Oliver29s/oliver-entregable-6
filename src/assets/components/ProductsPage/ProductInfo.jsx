3e90a6b20753efab9dd92dbaab45301641c0eae7
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-03-10 11:18:19 -0500

    solo faltan 4 horas mas css

--- src/assets/components/ProductsPage/ProductInfo.jsx
+++ src/assets/components/ProductsPage/ProductInfo.jsx
@@ -1,12 +1,17 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import confi from "../../layout/confi";
+import { getCartThunk } from "../../store/slice/cart.slice";
 import "./styles/productInfo.css";
 
 const ProductInfo = ({ product }) => {
   const [counter, setCounter] = useState(1);
   const [counterTwo, setcounterTwo] = useState(0);
+ const dispatch =  useDispatch()
 
   const handleBack = () => {
-    if (counterTwo  < 2) {
+    if (counterTwo < 2) {
       setcounterTwo(counterTwo + 1);
     }
   };
@@ -17,17 +22,28 @@ const ProductInfo = ({ product }) => {
   };
 
   const handlePlus = () => {
-    if (counter >= 2) {
-      setCounter((counter = 0));
-    } else {
-      setCounter(counter + 1);
-    }
+    setCounter(counter + 1);
   };
   const handleminus = () => {
     if (counter > 0) {
       setCounter(counter - 1);
     }
   };
+
+  const handleAdd = () =>{
+    const url ='https://e-commerce-api-v2.academlo.tech/api/v1/cart'
+    const data ={
+      quantity:counter,
+      productId:product?.id,
+    }
+    axios
+    .post(url,data,confi)
+    .then(res => {console.log(res.data)
+    dispatch(getCartThunk())
+    })
+    .catch(err => console.log(err.response))
+    setCounter(1)
+  }
 
   return (
     <section className="section__main">
@@ -38,26 +54,25 @@ const ProductInfo = ({ product }) => {
         <button onClick={handleBack}> {">"} </button>
       </div>
       <main className="section__mainTwo">
+        <h2>{product?.title}</h2>
 
-      <h2>{product?.title}</h2>
-
-      <div className="info__quantity">
-        <article className="info__articleOne">
-          <h4>Price</h4>
-          <span>{product?.price}</span>
-        </article>
-        <article className="info__articleTwo">
-          <h4>Quantity: {counter}</h4>
-          <div className="info__button">
-            <button onClick={handleminus}>-</button>
-            <button onClick={handlePlus}>+</button>
-          </div>
-        </article>
-      </div>
-      <button className="info_add">
-        Add to cart <i className="bx bxs-cart-add adds"></i>
-      </button>
-      <p>{product?.description}</p>
+        <div className="info__quantity">
+          <article className="info__articleOne">
+            <h4>Price</h4>
+            <span>{product?.price}</span>
+          </article>
+          <article className="info__articleTwo">
+            <h4>Quantity: {counter}</h4>
+            <div className="info__button">
+              <button onClick={handleminus}>-</button>
+              <button onClick={handlePlus}>+</button>
+            </div>
+          </article>
+        </div>
+        <button className="info_add" onClick={handleAdd}>
+          Add to cart <i className="bx bxs-cart-add adds"></i>
+        </button>
+        <p>{product?.description}</p>
       </main>
     </section>
   );
