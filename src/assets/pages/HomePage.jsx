116953d66563bdec567cb5144a9ac7e964580212
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-03-15 14:07:20 -0500

    css

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CardProduct from "../components/CardProduct";
+import "./styles-pages/homePage.css";
 import {
   getProductsByName,
   setAllProductsThunk,
@@ -43,58 +44,66 @@ const HomePage = () => {
     });
   };
   return (
-    <div className="card__main">
-      <div>
-      <form onSubmit={submit}  className="form__search">
+    <>
+      <div className="home__search">
         <input type="text" id="search" />
         <button>
           <i className="bx bx-search-alt"></i>
         </button>
-      </form>
-      <form onSubmit={onSubmit} className="form__price">
-        <h3>
-          Price <i className="bx bxs-chevron-down"></i>
-        </h3>
-        <div>
-          <label htmlFor="from">
-            {" "}
-            From:
-            <input type="number" id="from" required />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="to">
-            {" "}
-            To:
-            <input type="number" id="to" required />
-          </label>
-        </div>
-        <button>Filter Price</button>
-      </form>
-     
-      <article>
-        <h3>
-          Category <i className="bx bxs-chevron-down"></i>
-        </h3>
-        <ul>
-          <li onClick={product}>All Products</li>
-          {categorys?.map((prod) => (
-            <li key={prod.id} onClick={() => category(prod.id)}>
-              {prod.name}
-            </li>
-          ))}
-        </ul>
-      </article>
       </div>
+      <div className="card__main">
+        <form onSubmit={submit} className="form__search"></form>
+        <div className="filter__modal">
+          <form onSubmit={onSubmit} className="form__price">
+            <button className="button__filter x">X</button>
+            <h3 className="form__h3">
+              Price <i className="bx bxs-chevron-down"></i>
+            </h3>
+            <div className="form__label">
+              <label htmlFor="from">
+                {" "}
+                From:
+                <input type="number" id="from" required />
+              </label>
+            </div>
+            <div className="form__label">
+              <label htmlFor="to">
+                {" "}
+                To:
+                <input type="number" id="to" required />
+              </label>
+            </div>
+            <button className="button__filter">Filter Price</button>
+          </form>
+          <article className="form__article">
+            <h3>
+              Category <i className="bx bxs-chevron-down"></i>
+            </h3>
+            <ul className="home__ul">
+              <li onClick={product}>All Products</li>
+              {categorys?.map((prod) => (
+                <li key={prod.id} onClick={() => category(prod.id)}>
+                  {prod.name}
+                </li>
+              ))}
+            </ul>
+          </article>
+        </div>
 
-      {products?.length === 0 ? (
-        <h1>Not Product</h1>
-      ) : (
-        products?.filter(price => price.price >= inputPrice.from && price.price <= inputPrice.to).map((product) => (
-          <CardProduct key={product.id} product={product} />
-        ))
-      )}
-    </div>
+        {products?.length === 0 ? (
+          <h1>Not Product</h1>
+        ) : (
+          products
+            ?.filter(
+              (price) =>
+                price.price >= inputPrice.from && price.price <= inputPrice.to
+            )
+            .map((product) => (
+              <CardProduct key={product.id} product={product} />
+            ))
+        )}
+      </div>
+    </>
   );
 };
 
